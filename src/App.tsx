5ce25e760f7fb1ac7d5e4f4b11384ff4c5fdbf82
sitaladhikari002@gmail.com <sitaladhikari002@gmail.com>
2023-06-05 22:17:55 +0545

    update:draggable element

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { DragEvent, DragEventHandler, useRef, useState } from 'react'
 import './App.css'
+import Draggable, { DraggableCore } from 'react-draggable';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [editMode, seteditMode] = useState<boolean>(false);
+  const [positionX, setpositionX] = useState(0)
+  const [positionY, setpositionY] = useState(0)
+  const textRef = useRef<HTMLHeadingElement>(null) 
+  const [textMessage, settextMessage] = useState<string>("Enter the text")
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{backgroundColor:"red",height:"100vh"}} >
+    <h1>Create your meme</h1>
+
+    <Draggable>
+      {
+        editMode ? 
+        <input type="text" placeholder='Enter the text' value={textMessage} 
+        autoFocus
+        onChange={e=>settextMessage(e.currentTarget.value)}
+        onKeyDown={e=>{
+          console.log(e)
+          if(e.key === "Enter"){
+            seteditMode(false)
+          }
+        }}
+        
+        /> :
+        <h4  onDoubleClick={()=>seteditMode(true)}  >{textMessage}</h4> 
+ 
+      }
+    </Draggable>
+    </div>
+   
+  </>
   )
 }
 
